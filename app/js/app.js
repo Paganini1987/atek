a3ec38228f68959ddf7fc02db0fb3faa3f8e0b30
Pavel <webmkirov@gmail.com>
2020-02-27 12:21:47 +0300

    desctop done

--- app/js/app.js
+++ app/js/app.js
@@ -20,6 +20,15 @@ document.addEventListener("DOMContentLoaded", function() {
 		$('.js-main-carousel').trigger('next.owl.carousel')
 	})
 
+	//Слайдер в карточке магазина
+	$('.js-shops-carousel').owlCarousel({
+		items: 1,
+		loop: true,
+		smartSpeed: 500,
+		dots: true,
+		nav: false
+	})
+
 	//Слайдер акции
 	$('.js-sale-carousel').owlCarousel({
 		items: 5,
@@ -30,5 +39,41 @@ document.addEventListener("DOMContentLoaded", function() {
 		autoWidth: true,
 		dots: false,
 		nav: false
+	})
+
+	//Попап карты
+	$('.js-open-map-popup').on('click', function(e) {
+		e.preventDefault()
+
+		$('body').addClass('mapOpened')
+	})
+
+	$('.js-close-map-popup').on('click', function(e) {
+		e.preventDefault()
+
+		$('body').removeClass('mapOpened')
+	})
+
+	$('.df-popup__wrap').on('click', function(e) {
+		e.preventDefault()
+
+		if ($(e.target).closest('.df-popup__center').length === 0) {
+			$('body').removeClass('mapOpened')
+		}
+	})
+
+	//Go to top
+	$('.js-go-to-top').on('click', function(e) {
+		e.preventDefault()
+
+		$('html, body').animate({ scrollTop: 0 }, 500)
+	})
+
+	$(window).on('scroll', function () {
+		if ($(window).scrollTop() > $(window).innerHeight()/4) {
+			$('.js-go-to-top').addClass('active')
+		} else {
+			$('.js-go-to-top').removeClass('active')
+		}
 	})
 });
